4a2619e7dd7d926a09b2b9b93688783d3e81d7df
Juan Barezzi <juan.barezzi@gmail.com>
2022-09-11 20:43:48 -0300

    refactor: force PORT to number

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express, { json } from "express";
 import "express-async-errors";
 import dotenv from "dotenv";
 import cors from "cors";
-import router from "./routers";
+import { router } from "./routers";
 import errorHandler from "middlewares/errorHandler";
 
 dotenv.config();
@@ -14,5 +14,5 @@ app.use(json());
 app.use(router);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT: number = Number(process.env.PORT) || 5000;
 app.listen(PORT);
