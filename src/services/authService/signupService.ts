4eaa5d5a9ee58c1b14f5dc2ffb0b29410fb53e7c
Juan Barezzi <juan.barezzi@gmail.com>
2022-09-12 01:41:08 -0300

    feat: implement signup service

---
+++ src/services/authService/signupService.ts
@@ -0,0 +1,29 @@
+import { User } from "@prisma/client";
+import bcrypt from "bcrypt";
+import { ISign } from "../../interfaces/authInterfaces";
+import * as authRepository from "../../repositories/authRepository";
+import * as errorFactory from "../../utils/errorFactory";
+
+export async function signup(newUser: ISign) {
+  await verifyIfEmailExists(newUser.email);
+  newUser.password = await encryptPassword(newUser.password);
+  await createUser(newUser);
+  await authRepository.insert(newUser);
+}
+
+async function verifyIfEmailExists(email: string) {
+  const hasUser: User | null = await authRepository.findByEmail(email);
+  if (hasUser !== null) {
+    throw errorFactory.conflictError(
+      "There's already a user registered with this email."
+    );
+  }
+}
+
+async function encryptPassword(password: string) {
+  const saltRounds = 10;
+  const encryptedPassword = bcrypt.hash(password, saltRounds);
+  return encryptedPassword;
+}
+
+async function createUser(newUser: ISign) {}
