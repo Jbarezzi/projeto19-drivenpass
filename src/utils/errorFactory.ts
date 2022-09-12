40deee929c43c914b14e6f167c1ef9d95441256e
Juan Barezzi <juan.barezzi@gmail.com>
2022-09-12 00:26:00 -0300

    refactor: change object literals to named export

--- src/utils/errorFactory.ts
+++ src/utils/errorFactory.ts
@@ -1,36 +1,27 @@
-function notFoundError(entity: string) {
+export function notFoundError(entity: string) {
   return {
     type: "error_not_found",
     message: `Não foi possível encontrar ${entity}.`,
   };
 }
 
-function conflictError(message: string) {
+export function conflictError(message: string) {
   return {
     type: "error_conflict",
     message,
   };
 }
 
-function unauthorizedError(entity: string) {
+export function unauthorizedError(entity: string) {
   return {
     type: "error_unauthorized",
     message: `${entity} está incorreto.`,
   };
 }
 
-function unprocessableEntityError(messages: string[]) {
+export function unprocessableEntityError(messages: string[]) {
   return {
     type: "error_unprocessable_entity",
     message: messages,
   };
 }
-
-const errorFactory = {
-  notFound: notFoundError,
-  conflict: conflictError,
-  unauthorized: unauthorizedError,
-  unprocessable: unprocessableEntityError,
-};
-
-export default errorFactory;
