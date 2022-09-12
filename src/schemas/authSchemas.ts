2815ae72204c4870e90e3e3209a00e5499f0028b
Juan Barezzi <juan.barezzi@gmail.com>
2022-09-12 01:40:48 -0300

    fix: remove rule message

--- src/schemas/authSchemas.ts
+++ src/schemas/authSchemas.ts
@@ -1,6 +1,6 @@
 import Joi from "joi";
 
 export const signupSchema = Joi.object({
-  email: Joi.string().email().trim().required().message("teste"),
+  email: Joi.string().email().trim().required(),
   password: Joi.string().trim().min(10).required(),
 });
