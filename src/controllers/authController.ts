2528194b88a21f6c3959d2b51d0abd107348032c
Juan Barezzi <juan.barezzi@gmail.com>
2022-09-12 01:56:03 -0300

    feat: implement signin controller

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,9 +1,15 @@
 import { Request, Response } from "express";
 import { ISign } from "../interfaces/authInterfaces";
-import { authService } from "../services/authService";
+import * as authService from "../services/authService";
 
 export async function signup(req: Request, res: Response) {
   const newUser: ISign = req.body;
   await authService.signup(newUser);
+  res.sendStatus(204);
+}
+
+export async function signin(req: Request, res: Response) {
+  const newUser: ISign = req.body;
+  await authService.signin(newUser);
   res.sendStatus(204);
 }
