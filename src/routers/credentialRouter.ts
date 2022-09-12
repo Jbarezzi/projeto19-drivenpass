0b2357d30ce5c05dafb4a36dcf48e70877e91ef8
Juan Barezzi <juan.barezzi@gmail.com>
2022-09-12 11:06:04 -0300

    feat: add credential creation schema

--- src/routers/credentialRouter.ts
+++ src/routers/credentialRouter.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
 import { joiValidator } from "../middlewares/joiValidator";
 import * as credentialController from "../controllers/credentialController";
+import { credentialSchema } from "../schemas/credentialSchemas";
 
 export const credentialRouter = Router();
 
-credentialRouter.post("/credential", joiValidator, credentialController.create);
+credentialRouter.post(
+  "/credential",
+  joiValidator(credentialSchema),
+  credentialController.create
+);
