4bdc15946aff13d428ba1e37a8bd2e7f8a4c1c18
Juan Barezzi <juan.barezzi@gmail.com>
2022-09-12 01:49:35 -0300

    feat: create signin schema

--- src/schemas/authSchemas.ts
+++ src/schemas/authSchemas.ts
@@ -4,3 +4,8 @@ export const signupSchema = Joi.object({
   email: Joi.string().email().trim().required(),
   password: Joi.string().trim().min(10).required(),
 });
+
+export const signinSchema = Joi.object({
+  email: Joi.string().email().trim().required(),
+  password: Joi.string().trim().required(),
+});
