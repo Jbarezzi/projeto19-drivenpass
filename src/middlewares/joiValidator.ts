0b7fe7312c0f08c9637e677288556edc78e7eb6d
Juan Barezzi <juan.barezzi@gmail.com>
2022-09-12 00:28:09 -0300

    feat: add abortEarly config

--- src/middlewares/joiValidator.ts
+++ src/middlewares/joiValidator.ts
@@ -1,14 +1,14 @@
 import { NextFunction, Request, Response } from "express";
 import { Schema } from "joi";
-import errorFactory from "utils/errorFactory";
+import * as errorFactory from "utils/errorFactory";
 
 export const joiValidator = (schema: Schema) => {
   return (req: Request, _res: Response, next: NextFunction) => {
     const body: object = req.body;
-    const { error } = schema.validate(body);
+    const { error } = schema.validate(body, { abortEarly: false });
     if (error !== undefined) {
       const messages: string[] = error?.details.map((detail) => detail.message);
-      throw errorFactory.unprocessable(messages);
+      throw errorFactory.unprocessableEntityError(messages);
     }
     next();
   };
